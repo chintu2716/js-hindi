21beff0bf9921e1d9cffa0fa52692c833f08cc90
chintu2716 <90671589+chintu2716@users.noreply.github.com>
2023-10-04 07:33:13 +0000

    arrows done

--- 03_basics/04_arrow.js
+++ 03_basics/04_arrow.js
@@ -3,11 +3,35 @@ const user = {
     price:999,
 
     welcomeMessage: function(){
-          console.log(`${this.username} ,welcome to this page`);
+        //   console.log(`${this.username} ,welcome to this page`);
+        //   console.log(this);
     }
 }
 user.welcomeMessage()
 user.username="sam"
 user.welcomeMessage()
 
-console.log(this);
+// console.log(this);
+
+ function chai(){
+    let username="Dinesh"
+    console.log(this);
+ }
+
+ chai()
+
+// ********************************ArrowFunction*********************************
+const addTwoNum = (num1,num2)=> {
+   return (num1+num2)
+}
+const subTwoNum = (num1,num2)=>{return (num1-num2)}
+const mulTwoNum = (num1,num2)=> (num1*num2)
+
+console.log(addTwoNum(1,4));
+console.log(subTwoNum(10,4));
+console.log(mulTwoNum(20,5));
+
+const divTwoNum = (num1,num2)=> ({username:"dinesh"})
+console.log(divTwoNum(2,3));
+
+
