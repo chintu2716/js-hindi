c06cc11fa8c1cb9055c0a122b330f3548b56092c
chintu2716 <90671589+chintu2716@users.noreply.github.com>
2023-10-30 18:21:44 +0000

    dates_modified

--- 01_basics/02_dates.js
+++ 01_basics/02_dates.js
@@ -1,15 +1,15 @@
 
  let  myDate = new Date();
 
-//  console.log(myDate);
+ console.log(myDate);
 //  console.log(myDate.toString());
 //  console.log(myDate.toDateString());
 //  console.log(myDate.toLocaleString());
 
-   let newDate= new Date(2023,1,25,10,10)
+   // let newDate= new Date(2023,1,25,10,10)
 //    console.log(newDate.toLocaleString());
 
-   let YourDate = new Date("2023-03-21")
-   console.log(YourDate.toLocaleString());
+   // let YourDate = new Date("2023-03-21")
+   // console.log(YourDate.toLocaleString());
 
    
